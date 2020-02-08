c5380dd7dd4e2b27d867aea80e2dc69c2f8a321c
Jason Chiu <jason.chiu@inmobi.com>
2020-02-07 18:28:58 -0800

    Bump version of nodemon, add ability to loop through students

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
+const studentUtils = require('../db/util_students');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+/* Rendering student view */
+router.get('/', async (req, res, next) => {
+
+  try {
+    const students = await studentUtils.listAllStudentsDefaultSorted();
+    res.render('index', { title: 'Express', students: students});
+  } catch (err) {
+    next(err)
+  }
+
+  
 });
 
 module.exports = router;
