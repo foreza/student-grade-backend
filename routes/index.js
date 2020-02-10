f9ee8ac8d23cbbe2dc7050b89af2f145821e73b2
Jason Chiu <jforeza@gmail.com>
2020-02-09 21:38:54 -0800

    Fix sorting

--- routes/index.js
+++ routes/index.js
@@ -16,16 +16,15 @@ router.get('/', async (req, res, next) => {
       "title": 'Advanced!',
       "students": null,     // Store the returned / sorted students collection
       "darkMode": 0,        // Let the page know intended darkMode state
-      "sortDir": 0,         // Let the page know what the intended sort direction is
+      "sortDir": 1,         // Let the page know what the intended sort direction is
       "sortType": 0,        // Let the page know what the intended sort type is
-      "sortNameLink": `?sortType=name&sortDir=`,
-      "sortGradeLink": "?sortType=grade&sortDir=",
     }
 
         // Pick up all the possible query params
         renderParams.sortType = req.query.sortType;                 // sortTypes can be either "student" or "grade"
         renderParams.sortDir = Math.sign(req.query.sortDir);        // Cast it down to -1 or 1. -1 for ascend, +1 for descend
         renderParams.darkMode = Math.abs(req.query.darkMode) % 2;   // Cast it down to 1 or 0. 0 for no darkMode, 1 for darkMode
+
 
         if (renderParams.sortDir == 0) {
           renderParams.sortDir = 1;   // Set a default value
@@ -34,21 +33,15 @@ router.get('/', async (req, res, next) => {
         // Populate the students collection with the appropriate sort
         switch (renderParams.sortType) {
           case "name":
-            console.log(`sorting by name, ${renderParams.sortDir * -1}`);
-            renderParams.sortNameLink = `?sortType=name&sortDir=${renderParams.sortDir * -1}`
-            renderParams.students = await studentUtils.listAllStudentsSortedByName(renderParams.sortDir * -1);
+            renderParams.students = await studentUtils.listAllStudentsSortedByName(renderParams.sortDir);
             break;
           case "grade":
-            console.log(`sorting by grade, ${renderParams.sortDir * -1}`);
-            renderParams.students = await studentUtils.listAllStudentsSortedByGrade(renderParams.sortDir  * -1);
+            renderParams.students = await studentUtils.listAllStudentsSortedByGrade(renderParams.sortDir);
             break;
           default:
             console.log(`sorting by default`);
-
             renderParams.students = await studentUtils.listAllStudentsDefaultSorted();
         }
-
-        // TODO: Handle Darkmode
 
         res.render('index', renderParams);
 
@@ -71,10 +64,8 @@ router.get('/', async (req, res, next) => {
     let renderParams = {
       "title": 'Advanced!',
       "students": null,     // Store the returned / sorted students collection
-      "sortNameLink": "?sortType=name&sortDir=1",
-      "sortGradeLink": "?sortType=grade&sortDir=1",
       "darkMode": 0,        // Let the page know intended darkMode state
-      "sortDir": 0,         // Let the page know what the intended sort direction is
+      "sortDir": 1,         // Let the page know what the intended sort direction is
       "sortType": 0        // Let the page know what the intended sort type is
     }
 
