b443147875d3be6756c19452c97a1b4a35ebc1aa
Jason Chiu <jforeza@gmail.com>
2020-02-13 23:45:48 -0800

    Modify mocha test directory and npm command to use server-tests directory

---
+++ server-tests/test2.js
@@ -0,0 +1,25 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const assert = require('chai').assert;
+const app = require('../app');
+
+const studentModel = require('../models/studentModel')
+
+chai.use(chaiHttp);
+
+describe('Users', function() {
+  before(async function() {
+    // runs before all tests in this block
+    await studentModel.remove({});
+  });
+
+  it('Users db should be empty (2)', async function() {
+    try {
+      const response = await chai.request(app).get('/students');
+      assert.equal(response.body.length == 0, true, 'Results should be empty');
+    } catch (err) {
+      throw err;
+    }
+  });
+
+});
