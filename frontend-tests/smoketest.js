38e5fc702b1499bf2d6bec90d636876777c67bc6
Jason Chiu <jforeza@gmail.com>
2020-02-24 00:13:29 -0800

    Add some basic tests and handle native alert dialogue

--- frontend-tests/smoketest.js
+++ frontend-tests/smoketest.js
@@ -1,18 +1,37 @@
 import { Selector } from 'testcafe';
 fixture `Getting Started`
-    .page `http://localhost:3000/`;
+    .page `http://localhost:3000/basic_index.html`;
 
-    test('Test basic input', async t => {
+    test('Test basic input for a student', async t => {
         await t
             .typeText('#input-name', 'John Smith')
             .typeText('#input-grade', '99')
-            .click("#submit-new-student-btn")
-
+            .click("#submit-new-student")
+            .expect(Selector('#table-content').childElementCount).eql(1);
     });
 
-    test('Test basic input', async t => {
+    test('Test basic input for a second student', async t => {
         await t
             .typeText('#input-name', 'Yvonne Yuan')
             .typeText('#input-grade', '100')
-            .click("#submit-new-student-btn")
+            .click("#submit-new-student")
+            .expect(Selector('#table-content').childElementCount).eql(2);
+    });
+
+    test('Test basic input for an invalid student (grade exceeds 100)', async t => {
+        await t
+            .typeText('#input-name', 'Morton')
+            .typeText('#input-grade', '101')
+            .setNativeDialogHandler(() => true)
+            .click("#submit-new-student")
+            .expect(Selector('#table-content').childElementCount).eql(2);
+    });
+
+    test('Test basic input for an invalid student (grade is lower than 0)', async t => {
+        await t
+            .typeText('#input-name', 'Morton')
+            .typeText('#input-grade', '-1')
+            .setNativeDialogHandler(() => true)
+            .click("#submit-new-student")
+            .expect(Selector('#table-content').childElementCount).eql(2);
     });
